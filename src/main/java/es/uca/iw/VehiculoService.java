636a7e35673c6688fa3460b8670a21e0c0558c6a
JoseMari98 <josemari-98@hotmail.com>
2019-04-04 17:26:40 +0200

    Version 0.0.3

--- src/main/java/es/uca/iw/VehiculoService.java
+++ src/main/java/es/uca/iw/VehiculoService.java
@@ -2,9 +2,6 @@ package es.uca.iw;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 
 @Service
