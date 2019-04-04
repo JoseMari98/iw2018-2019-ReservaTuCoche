d0c1b93ae48a5746cbc7033764f483651ad8235a
Goonie19 <strawhatgoonie@gmail.com>
2019-04-04 17:17:29 +0200

    version 0.0.1

--- src/main/java/es/uca/iw/VehiculoService.java
+++ src/main/java/es/uca/iw/VehiculoService.java
@@ -10,11 +10,8 @@ import java.util.Optional;
 @Service
 public class VehiculoService {
 
-    private static VehiculoService instance;
-
-    private final HashMap<Long, Vehiculo> contacts = new HashMap<>();
-
     private VehiculoRepository repo;
+
     @Autowired
     private VehiculoService(VehiculoRepository repo) {
         this.repo = repo;
@@ -30,6 +27,14 @@ public class VehiculoService {
 
     public Iterable<Vehiculo> findAll() {
         return repo.findAll();
+    }
+
+    public Long count() {
+        return repo.count();
+    }
+
+    public void delete(Vehiculo entidad) {
+        repo.delete(entidad);
     }
 
 }
