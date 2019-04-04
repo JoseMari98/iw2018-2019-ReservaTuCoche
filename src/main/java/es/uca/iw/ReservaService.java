6c81463f4bb194ac5578af15baf576c759fae626
Goonie19 <strawhatgoonie@gmail.com>
2019-04-04 16:21:42 +0200

    version 0.0.3

--- src/main/java/es/uca/iw/ReservaService.java
+++ src/main/java/es/uca/iw/ReservaService.java
@@ -1,4 +1,13 @@
 package es.uca.iw;
 
-public class ReservaService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class ReservaService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
