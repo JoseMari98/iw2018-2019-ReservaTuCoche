0070ccedd509263299fafabd64da1dbcf7a41865
CarpioKey <luismi.klavier@gmail.com>
2019-04-05 22:03:23 +0200

    Añadido ReservaRepository. Añadida primera versión de ReservaService.

---
+++ src/main/java/es/uca/iw/ReservaRepository.java
@@ -0,0 +1,7 @@
+package es.uca.iw;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReservaRepository extends CrudRepository<Reserva, Long> {
+
+}
--- src/main/java/es/uca/iw/ReservaService.java
+++ src/main/java/es/uca/iw/ReservaService.java
@@ -1,6 +1,35 @@
 package es.uca.iw;
 
-public class ReservaService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
 
+@Service
+public class ReservaService {
+    private ReservaRepository repo;
+    @Autowired
+    private ReservaService(ReservaRepository repo) {
+        this.repo = repo;
+    }
+
+    public synchronized Reserva save(Reserva entrada) {
+        return repo.save(entrada);
+    }
+
+    public Optional<Reserva> findById(Long id) {
+        return repo.findById(id);
+    }
+
+    public Iterable<Reserva> findAll() {
+        return repo.findAll();
+    }
+
+    public Long count() {
+        return repo.count();
+    }
+
+    public void delete (Reserva entidad) {
+        repo.delete(entidad);
+    }
 
 }
