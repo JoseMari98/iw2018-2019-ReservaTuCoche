6d01f32ba1a39998350c6054ae99e423d164a1f9
CarpioKey <luismi.klavier@gmail.com>
2019-04-07 01:15:01 +0200

    Modificado ReservaForm para mejorar su funcionalidad (eventos para corregir fechas, validar checkbox y efecto de los botones).

--- src/main/java/es/uca/iw/ReservaForm.java
+++ src/main/java/es/uca/iw/ReservaForm.java
@@ -5,8 +5,11 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
+
+import java.time.LocalDate;
 
 @Route("reservaform")
 
@@ -22,10 +25,44 @@ public class ReservaForm extends FormLayout {
     public ReservaForm() {
         HorizontalLayout buttons = new HorizontalLayout(save, delete);
         save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        LocalDate actual = LocalDate.now();
+        fechaInicio.setMin(actual);
+        fechaInicio.setMax(actual.plusYears(1));
+        fechaFin.setMin(actual);
+        fechaFin.setMax(actual.plusYears(1));
         add(fechaInicio, fechaFin, validation, buttons);
 
         setSizeFull();
 
+        fechaInicio.addValueChangeListener(event -> {
+            LocalDate inicio = event.getValue();
+            LocalDate fin = fechaFin.getValue();
+            if (inicio != null) {
+                fechaFin.setMin(inicio.plusDays(1));
+            }
+        });
+
+        fechaFin.addValueChangeListener(event -> {
+            LocalDate fin = event.getValue();
+            LocalDate inicio = fechaInicio.getValue();
+            if (fin != null) {
+                fechaInicio.setMax(fin.minusDays(1));
+            }
+        });
+
+        save.addClickListener(click -> {
+            Div mensaje = new Div();
+            if (!validation.getValue()) {
+                mensaje.setText("Se deben aceptar los términos y condiciones para proceder con la reserva.");
+            } else {
+                mensaje.setText("Formulario correcto. Registro de la reserva pendiente de implementar.");
+            }
+        });
+
+        delete.addClickListener(click -> {
+            Div mensaje = new Div();
+            mensaje.setText("\"Volver a la página anterior\" pendiente de implementar.");
+        });
     }
 
 }
