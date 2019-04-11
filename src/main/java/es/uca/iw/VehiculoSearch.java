b428ded89e2923c94cfc61d642f34a5cfda7c474
Goonie19 <strawhatgoonie@gmail.com>
2019-04-11 16:30:15 +0200

    version 0.0.5 En esta version se extraen los coches de la base de datos y se muestran en el grid, aún no se puede buscar, pero se pueden mostrar todos los vehículos.

--- src/main/java/es/uca/iw/VehiculoSearch.java
+++ src/main/java/es/uca/iw/VehiculoSearch.java
@@ -13,20 +13,33 @@ import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 import static org.apache.el.lang.ELArithmetic.add;
 
-@Route("VehiculoSearch")
+@Route("Search")
 public class VehiculoSearch extends FormLayout {
 
     private Grid<Vehiculo> gVehiculos = new Grid<>(Vehiculo.class);
     private TextField filtertext = new TextField();
-    public VehiculoSearch() {
+    private VehiculoService service;
+    private Button search = new Button("Search!");
+
+    @Autowired
+    public VehiculoSearch(VehiculoService serv) {
+        service = serv;
+        ArrayList<Vehiculo> listaVehiculo = new ArrayList<>();
+        for(Vehiculo vehiculo : service.listarVehiculo())
+            listaVehiculo.add(vehiculo);
 
         filtertext.setPlaceholder("filtrar por texto");
         filtertext.setClearButtonVisible(true);
         filtertext.setValueChangeMode(ValueChangeMode.EAGER);
 
         gVehiculos.setColumns("marca", "modelo", "precio");
+
+        gVehiculos.setItems(listaVehiculo);
+
         VerticalLayout Busqueda = new VerticalLayout(filtertext, gVehiculos);
 
         add(Busqueda);
